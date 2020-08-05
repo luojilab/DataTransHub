ed5f05725b9658f030c9c0e0ae3db354035df7bd
lixiaoyu <lixiaoyu@luojilab.com>
2020-08-05 22:29:14 +0800

    fix(android):fix demo

--- build/Android/app/src/main/java/com/iget/demo/MainActivity.java
+++ build/Android/app/src/main/java/com/iget/demo/MainActivity.java
@@ -1,98 +1,120 @@
 package com.iget.demo;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
 import android.os.Handler;
 import android.os.Looper;
 import android.view.View;
 
+import androidx.appcompat.app.AppCompatActivity;
+
 import com.iget.datatranshub.DataTransHub;
 import com.iget.datatranshub.callback.IUploadCallback;
-import com.iget.datatranshub.demo.R;
 import com.iget.datatranshub.callback.IUploadImp;
+import com.iget.datatranshub.demo.R;
 import com.iget.flog.Flog;
 import com.iget.flog.Log;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
-    private DataTransHub dataTransHub;
+    private DataTransHub dataTransHub1;
+    private DataTransHub dataTransHub2;
     private Flog flog;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        findViewById(R.id.id_start).setOnClickListener(this);
-        findViewById(R.id.id_push).setOnClickListener(this);
-        findViewById(R.id.id_reweaken).setOnClickListener(this);
-        findViewById(R.id.id_manual_trigger_upload).setOnClickListener(this);
-        findViewById(R.id.id_release).setOnClickListener(this);
+        findViewById(R.id.id_start1).setOnClickListener(this);
+        findViewById(R.id.id_push1).setOnClickListener(this);
+        findViewById(R.id.id_reweaken1).setOnClickListener(this);
+        findViewById(R.id.id_manual_trigger_upload1).setOnClickListener(this);
+        findViewById(R.id.id_release1).setOnClickListener(this);
         findViewById(R.id.id_test_flog).setOnClickListener(this);
+
+        findViewById(R.id.id_start2).setOnClickListener(this);
+        findViewById(R.id.id_push2).setOnClickListener(this);
+        findViewById(R.id.id_reweaken2).setOnClickListener(this);
+        findViewById(R.id.id_manual_trigger_upload2).setOnClickListener(this);
+        findViewById(R.id.id_release2).setOnClickListener(this);
     }
 
     @Override
     public void onClick(View v) {
         switch (v.getId()) {
-            case R.id.id_start:
-                start();
+            case R.id.id_start1:
+                start1();
                 break;
-            case R.id.id_reweaken:
-                testReweaken();
+            case R.id.id_reweaken1:
+                testReweaken1();
                 break;
-            case R.id.id_manual_trigger_upload:
-                testManualTriggerUpload();
+            case R.id.id_manual_trigger_upload1:
+                testManualTriggerUpload1();
                 break;
-            case R.id.id_push:
-                testPush();
+            case R.id.id_push1:
+                testPush1();
                 break;
-            case R.id.id_release:
-                testRelease();
+            case R.id.id_release1:
+                testRelease1();
                 break;
             case R.id.id_test_flog:
                 testFlog();
+                break;
+            case R.id.id_start2:
+                start2();
+                break;
+            case R.id.id_reweaken2:
+                testReweaken2();
+                break;
+            case R.id.id_manual_trigger_upload2:
+                testManualTriggerUpload2();
+                break;
+            case R.id.id_push2:
+                testPush2();
+                break;
+            case R.id.id_release2:
+                testRelease2();
                 break;
 
         }
     }
 
-    public void start() {
-        String dataTransHubCacheDir = this.getFilesDir().getAbsolutePath() + "/data_trans_hub_cache";
-        String dataTransHubDataDir = this.getFilesDir().getAbsolutePath() + "/data_trans_hub_data";
-        dataTransHub = DataTransHub.makeDataTransHub(dataTransHubCacheDir, dataTransHubDataDir, "572d1e2710ae5fbca54c76a382fdd44050b3a675cb2bf39feebe85ef63d947aff0fa4943f1112e8b6af34bebebbaefa1a0aae055d9259b89a1858f7cc9af9df1");
-        dataTransHub.setUploadTriggerWayWay(DataTransHub.UploadTriggerWayWayAuto);
-        dataTransHub.setUploadImp(new IUploadImp() {
+    public void start1() {
+        String dataTransHubCacheDir = this.getFilesDir().getAbsolutePath() + "/data_trans_hub_cache1";
+        String dataTransHubDataDir = this.getFilesDir().getAbsolutePath() + "/data_trans_hub_data1";
+        dataTransHub1 = DataTransHub.makeDataTransHub(dataTransHubCacheDir, dataTransHubDataDir, "572d1e2710ae5fbca54c76a382fdd44050b3a675cb2bf39feebe85ef63d947aff0fa4943f1112e8b6af34bebebbaefa1a0aae055d9259b89a1858f7cc9af9df1");
+        dataTransHub1.setUploadTriggerWayWay(DataTransHub.UploadTriggerWayWayAuto);
+        dataTransHub1.setUploadImp(new IUploadImp() {
             @Override
             public void upload(final String filePath) {
-                Log.d("test","file:" + filePath);
+                Log.d("test","dth1 file:" + filePath);
                 Handler uiHandler = new Handler(Looper.getMainLooper());
                 uiHandler.post(new Runnable() {
                     @Override
                     public void run() {
-                        dataTransHub.notifyUploadSuccess(filePath);
+                        dataTransHub1.notifyUploadSuccess(filePath);
                     }
                 });
             }
         });
         //1 day
-        dataTransHub.setExpiredTime(60 * 60 * 24);
+        dataTransHub1.setExpiredTime(60 * 60 * 24);
         //10 min
-        dataTransHub.setReportingInterval(1000 *60);
-        dataTransHub.setFileMaxSize(1024 * 200);
-        dataTransHub.start();
+        dataTransHub1.setReportingInterval(1000 *60);
+        dataTransHub1.setFileMaxSize(1024 * 200);
+        dataTransHub1.start();
     }
 
-    private void testPush() {
+    private void testPush1() {
         for (int i = 0; i < 10000; i++) {
-            dataTransHub.push("ev=s_paid_paid_impression&uid=12005419&scr=1080*2214&t=1547627349367082203&seid=dd86a82b76722c24427b9db1fb462a4d&net=wifi&mac=c6abbef9f4bea0a0&sid=dd86a82b76722c24427b9db1fb462a4d".getBytes());
+            dataTransHub1.push("ev=s_paid_paid_impression&uid=12005419&scr=1080*2214&t=1547627349367082203&seid=dd86a82b76722c24427b9db1fb462a4d&net=wifi&mac=c6abbef9f4bea0a0&sid=dd86a82b76722c24427b9db1fb462a4d".getBytes());
         }
     }
 
-    private void testReweaken() {
-        dataTransHub.reaWaken();
+    private void testReweaken1() {
+        dataTransHub1.reaWaken();
     }
 
-    private void testManualTriggerUpload() {
-        dataTransHub.manualTriggerUpload(new IUploadCallback() {
+    private void testManualTriggerUpload1() {
+        dataTransHub1.manualTriggerUpload(new IUploadCallback() {
             @Override
             public void finish() {
                 Log.d("", "");
@@ -100,9 +122,60 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         });
     }
 
-    private void testRelease() {
-        if(dataTransHub != null){
-            dataTransHub.release();
+    private void testRelease1() {
+        if(dataTransHub1 != null){
+            dataTransHub1.release();
+        }
+    }
+
+    public void start2() {
+        String dataTransHubCacheDir = this.getFilesDir().getAbsolutePath() + "/data_trans_hub_cache2";
+        String dataTransHubDataDir = this.getFilesDir().getAbsolutePath() + "/data_trans_hub_data2";
+        dataTransHub2 = DataTransHub.makeDataTransHub(dataTransHubCacheDir, dataTransHubDataDir, "572d1e2710ae5fbca54c76a382fdd44050b3a675cb2bf39feebe85ef63d947aff0fa4943f1112e8b6af34bebebbaefa1a0aae055d9259b89a1858f7cc9af9df1");
+        dataTransHub2.setUploadTriggerWayWay(DataTransHub.UploadTriggerWayWayAuto);
+        dataTransHub2.setUploadImp(new IUploadImp() {
+            @Override
+            public void upload(final String filePath) {
+                Log.d("test","dth2 file:" + filePath);
+                Handler uiHandler = new Handler(Looper.getMainLooper());
+                uiHandler.post(new Runnable() {
+                    @Override
+                    public void run() {
+                        dataTransHub2.notifyUploadSuccess(filePath);
+                    }
+                });
+            }
+        });
+        //1 day
+        dataTransHub2.setExpiredTime(60 * 60 * 24);
+        //10 min
+        dataTransHub2.setReportingInterval(1000 *60);
+        dataTransHub2.setFileMaxSize(1024 * 200);
+        dataTransHub2.start();
+    }
+
+    private void testPush2() {
+        for (int i = 0; i < 10000; i++) {
+            dataTransHub2.push("ev=s_paid_paid_impression&uid=12005419&scr=1080*2214&t=1547627349367082203&seid=dd86a82b76722c24427b9db1fb462a4d&net=wifi&mac=c6abbef9f4bea0a0&sid=dd86a82b76722c24427b9db1fb462a4d".getBytes());
+        }
+    }
+
+    private void testReweaken2() {
+        dataTransHub2.reaWaken();
+    }
+
+    private void testManualTriggerUpload2() {
+        dataTransHub2.manualTriggerUpload(new IUploadCallback() {
+            @Override
+            public void finish() {
+                Log.d("", "");
+            }
+        });
+    }
+
+    private void testRelease2() {
+        if(dataTransHub2 != null){
+            dataTransHub2.release();
         }
     }
 
